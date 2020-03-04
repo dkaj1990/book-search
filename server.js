7b43ab7a4ca5ab21abd99d2bdc6e58fe7258b44d
Dipti Katkar <dk.aj1990@gmail.com>
2020-03-03 18:54:13 -0600

    Initial setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const path = require('path'); 
+const PORT = process.env.PORT || 9090;
+const app = express(); 
+
+app.use(express.urlencoded({extended:true})); 
+app.use(express.json()); 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, ()=> {
+    console.log('API server now on port ' + PORT);
+});
