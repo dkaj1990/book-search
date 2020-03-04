0a43faba4e88f88ce959ddc49eeb719543928226
Dipti Katkar <dk.aj1990@gmail.com>
2020-03-03 19:32:04 -0600

    Added mongoose and models

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path'); 
 const PORT = process.env.PORT || 9090;
 const app = express(); 
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 app.use(express.urlencoded({extended:true})); 
 app.use(express.json()); 
@@ -13,6 +15,10 @@ if(process.env.NODE_ENV === 'production'){
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books");
 
 app.listen(PORT, ()=> {
     console.log('API server now on port ' + PORT);
